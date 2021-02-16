613cc444ae121e80017ce83f14e4e3fe08343345
Russell Jones <jonesrussell42@gmail.com>
2021-02-15 21:11:15 -0500

    Drop old redis settings

--- config/database.php
+++ config/database.php
@@ -140,34 +140,25 @@ return [
 
         'client' => env('REDIS_CLIENT', 'phpredis'),
 
-        'options' => [
-            'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env(
-                'REDIS_PREFIX',
-                Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'
-            ),
-            'parameters' => ['password' => env('REDIS_PASSWORD', null)],
-        ],
-
         'default' => [
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
-            'port' => env('REDIS_PORT', '6379'),
-            'database' => env('REDIS_DB', '0'),
+            'port' => env('REDIS_PORT', 6379),
+            'database' => env('REDIS_DB', 0),
         ],
 
         'cache' => [
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
-            'port' => env('REDIS_PORT', '6379'),
-            'database' => env('REDIS_CACHE_DB', '1'),
+            'port' => env('REDIS_PORT', 6379),
+            'database' => env('REDIS_CACHE_DB', 1),
         ],
 
         'content' => [
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
-            'port' => env('REDIS_PORT', '6379'),
-            'database' => env('REDIS_CONTENT_DB', '2'),
+            'port' => env('REDIS_PORT', 6379),
+            'database' => env('REDIS_CONTENT_DB', 2),
         ],
 
     ],
