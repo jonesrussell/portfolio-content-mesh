210c380df60a97a1150638449f6799eed15c6198
Russell Jones <jonesrussell42@gmail.com>
2021-05-04 19:16:16 -0400

    Use moloquent

--- config/database.php
+++ config/database.php
@@ -154,11 +154,18 @@ return [
             'database' => env('REDIS_CACHE_DB', 1),
         ],
 
+        'queue' => [
+            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'password' => env('REDIS_PASSWORD', null),
+            'port' => env('REDIS_PORT', 6379),
+            'database' => env('REDIS_QUEUE_DB', 2),
+        ],
+
         'content' => [
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
             'port' => env('REDIS_PORT', 6379),
-            'database' => env('REDIS_CONTENT_DB', 2),
+            'database' => env('REDIS_CONTENT_DB', 3),
         ],
 
     ],
